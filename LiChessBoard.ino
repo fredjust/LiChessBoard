f69d5788d1c60aede83f76057b90a3c494d08d61
fredjust <fredjust@gmail.com>
2018-06-10 15:35:31 +0200

    ancienne source

---
+++ LiChessBoard.ino
@@ -0,0 +1,200 @@
+//-----------------------------------------------------------
+// GESTION DU Pseudo CLAVIER 8x8
+//-----------------------------------------------------------
+
+#include "Key.h"
+#include "Keypad.h"
+
+// GESTION des LED
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
+
+bool etatchange = false;
+
+//define the cymbols on the buttons of the keypads
+char hexaKeys[8][8] = {
+  {'1', '2', '3', '4', '5', '6', '7', '8'},
+  {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
+  {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'},
+  {'&', 'B', '#', '{', '(', '-', 'B', '_'},
+  {'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'},
+  {'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'},
+  {'q', 'r', 's', 't', 'u', 'v', 'w', 'x'},
+  {'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X'}
+};
+
+byte rowPins[8] = {12, 11, 10, 9, 8, 7, 6, 13}; //connect to the row pinouts of the keypad
+byte colPins[8] = {14, 15, 16, 17, 4, 3, 2, 5}; 
+
+byte NewbitMap[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+//initialize an instance of class NewKeypad
+Keypad ChessBoard = Keypad( makeKeymap(hexaKeys), rowPins, colPins, 8, 8);
+
+unsigned long startTime;
+
+String strPos = "0.0.0.0.0.0.0.0.";
+int nbPiece = 0;
+
+//-----------------------------------------------------------
+// effectue les permutations de bits pour renvoyer les bonnes signatures plateau
+//-----------------------------------------------------------
+void PermuToBOARD() {
+  byte TempoBitMap = 0;
+  strPos = "";
+  nbPiece=0;
+  for (int i = 7; i >= 0; i--)
+  {
+    for (int b = 7; b >= 0; b--)
+    {
+      // bitRead(ChessBoard.bitMap[gauche/droite b/7-b],blancs/noirs i/7-i)           
+      bitWrite( TempoBitMap, b, bitRead(ChessBoard.bitMap[7 - b], 7-i) );   
+      nbPiece=nbPiece+bitRead(ChessBoard.bitMap[7-b],7-i);
+    }
+    strPos = strPos + TempoBitMap;
+    strPos = strPos + ".";
+    NewbitMap[7 - i] = TempoBitMap;    
+  }    
+}
+
+//-----------------------------------------------------------
+// animation de départ lorsque la position de départ est OK
+//-----------------------------------------------------------
+byte ImReady() {
+  for (int i = 0; i < 8; i++) {
+    matrix.clear();
+    for (int j = 0; j < 8; j++) {
+      matrix.drawPixel(7 - i, j, LED_ON);
+    }
+    matrix.writeDisplay();  // write the changes we just made to the display
+    delay(100);
+  }
+  for (int i = 0; i < 8; i++) {
+    matrix.clear();
+    for (int j = 0; j < 8; j++) {
+      matrix.drawPixel(i, j, LED_ON);
+    }
+    matrix.writeDisplay();  // write the changes we just made to the display
+    delay(100);
+  }
+  matrix.clear();
+  matrix.writeDisplay();
+  return 0;
+}
+
+//******************************************************************************************
+//                          SETUP
+//******************************************************************************************
+void setup()
+{
+  //initialisation du port serie
+  Serial.begin(115200);
+  //Serial.println("HELLO WORLD");  
+  
+  //anti rebond et appuis long
+  ChessBoard.setDebounceTime(10);
+  ChessBoard.setHoldTime(2000);
+  ChessBoard.getKeys();
+  PermuToBOARD();  
+  
+  //initialise la matrice
+  matrix.begin(0x70);  
+  matrix.setRotation(3);
+  matrix.clear();      
+  matrix.writeDisplay();
+  //matrix.setBrightness(16);
+
+  //-----------------------------------------------------------
+  //  Allume les cases qui contiennent une piece
+  // lorsuqu'il y a moins de 16 pieces
+  //-----------------------------------------------------------  
+  while (nbPiece < 17)
+  {
+    if ( ChessBoard.getKeys() )
+    {
+      PermuToBOARD();           
+      matrix.clear();
+      matrix.drawBitmap(0, 0, NewbitMap, 8, 8, LED_ON);
+      matrix.writeDisplay();      
+      Serial.println(strPos);               
+      Serial.println(nbPiece);     
+    }
+  }    
+  for (int i=0;i<8;i++) { NewbitMap[i]=abs(195-NewbitMap[i]); } //inversion de la signature
+  matrix.clear();
+  matrix.drawBitmap(0, 0, NewbitMap, 8, 8, LED_ON);
+  matrix.writeDisplay();     
+      
+  //-----------------------------------------------------------
+  //  Allume les cases de la position de départ
+  //  qui ne contiennent pas de piece
+  //-----------------------------------------------------------
+  while (strPos !="195.195.195.195.195.195.195.195.")
+  {
+    if ( ChessBoard.getKeys() )
+    {
+      PermuToBOARD();
+      //inversion de la signature
+      for (int i=0;i<8;i++) { NewbitMap[i]=abs(195-NewbitMap[i]); }
+      matrix.clear();
+      matrix.drawBitmap(0, 0, NewbitMap, 8, 8, LED_ON);
+      matrix.writeDisplay();      
+      Serial.println(strPos);               
+      Serial.println(nbPiece);     
+    }
+  }  
+  // il est pret lance l'animation
+  ImReady();
+}
+
+//******************************************************************************************
+//                          LOOP
+//******************************************************************************************
+
+void loop() {
+  char carlu = 0;
+
+  // si on recoit des infos sur le port serie
+
+  while (Serial.available() > 0)
+  { // tant que des caractères sont en attente
+    delay(10);
+    carlu = Serial.read();
+    //Serial.print(carlu); //puis on le renvoi à l’expéditeur tel quel
+    if (carlu == 'c')
+    {
+      matrix.clear();
+    }
+    if (carlu == 'o')
+    {
+      matrix.clear();
+      int ligne = Serial.parseInt();
+      int colonne = Serial.parseInt();
+      if (ligne > 0 && ligne < 9 && colonne > 0 && colonne < 9)
+      {
+        matrix.drawPixel(8 - ligne, colonne - 1, LED_ON);
+      }
+      ligne = Serial.parseInt();
+      colonne = Serial.parseInt();
+      if (ligne > 0 && ligne < 9 && colonne > 0 && colonne < 9)
+      {
+        matrix.drawPixel(8 - ligne, colonne - 1, LED_ON);
+      };
+    }
+    matrix.writeDisplay();
+  }
+
+  if ( ChessBoard.getKeys() )
+  {
+    PermuToBOARD();
+   
+    //affiche les infos sur le port serie
+    Serial.println( millis() - startTime ); 
+    Serial.println(strPos);                 
+    startTime = millis();
+  }
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# lichessboard
+ChessboARDuino with nano card and LED matrix
+
+programme pour controler un lichessboard (chessboarduino + matrice de LED) 
+
+
+
+
+
